873556a564866cfa11313b730bf5df26d5e6abfa
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2026-01-26 12:00:08 +0200

    Add connectionstrings to slqserver, Add CarsDto and Cars Domain

---
+++ Cars.Core/Domain/Cars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Core.Domain
+{
+    public class Cars
+    {
+        public Guid? Id { get; set; }
+        public string? Brand {  get; set; }
+        public string? Model { get; set; }
+        public string? FuelType { get; set; }
+        public int? Power { get; set; }
+        public string? Drivetrain { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get;set; }
+    }
+}
