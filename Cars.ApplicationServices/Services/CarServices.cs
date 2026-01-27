bb1e8dba5c58af607b83022be998635e501bc1d5
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2026-01-27 10:34:17 +0200

    Fix proj references, Add CarServices Update/Create

---
+++ Cars.ApplicationServices/Services/CarServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cars.Core.ServiceInterface;
+using Cars.Data;
+
+namespace Cars.ApplicationServices.Services
+{
+    public class CarServices : ICarServices
+    {
+        private readonly CarsTARge24Context _context;
+    }
+}
---
+++ CarsTARge24/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarsTARge24.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
