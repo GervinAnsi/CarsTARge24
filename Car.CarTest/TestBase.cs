0e6c3681f486a1f66f4e7ae0f75326c235f81997
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2026-02-01 16:50:57 +0200

    Create TestBase

---
+++ Car.CarTest/TestBase.cs
@@ -0,0 +1,19 @@
+﻿using Cars.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace Cars.CarTest
+{
+    public abstract class TestBase
+    {
+
+        protected CarsTARge24Context GetInMemoryDb()
+        {
+            var options = new DbContextOptionsBuilder<CarsTARge24Context>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new CarsTARge24Context(options);
+        }
+    }
+}
